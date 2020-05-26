7288aaa89477cd09a829a7486315f7a5b9350fe3
Amannor <alon.mannor@gmail.com>
2020-05-26 23:15:18 +0300

    Updated part2 files

--- myCode/submission/part2/k_cluster.py
+++ myCode/submission/part2/k_cluster.py
@@ -4,23 +4,20 @@ import matplotlib.pyplot as plt
 import numpy as np
 from sklearn.metrics import silhouette_score
 import ast
+import os
 
+dir_path = os.path.dirname(os.path.realpath(__file__))
+full_path_for_current_distances_file = os.path.join(dir_path,'distance_to_articles_ids.json')
 
-full_path_for_current_distances_file = r'C:\idc\AdvancedML\ex2_and_relevant_lectures\myCode\take1\distance_to_articles_ids.json'
 with open(full_path_for_current_distances_file, 'r') as myfile:
     distance_to_articles_ids=json.load(myfile)
 print(f'finished loading distance_to_articles_ids')
 X = distance_to_articles_ids.keys()
 X = list(map(ast.literal_eval, X))
-# print(f"X[0] {X[0]}")
-# print(f"type(X[0]) {type(X[0])}")
-# v = ast.literal_eval(X[0])
-# print(f"type(v) {type(v)}")
-
-# X = list(map(lambda d: tuple(d), distance_to_articles_ids.keys()))
+best_cluster_num_to_silhouette = (-2, -2)
 X = np.array(X)
-# print(f"X {X}")
-for clusters_num in range(2,10):
+for clusters_num in range(2,11):
+	print(f"Computing {clusters_num} clusters")
 
 	kmeans = KMeans(n_clusters=clusters_num)
 	kmeans.fit(X)
@@ -32,7 +29,8 @@ for clusters_num in range(2,10):
 	plt.scatter(centers[:, 0], centers[:, 1], c='black', s=200, alpha=0.5)
 	# plt.show()
 
-	# clusterer = KMeans(n_clusters=clusters_num, random_state=10)
-	# cluster_labels = clusterer.fit_predict(X)
 	silhouette_avg = silhouette_score(X, y_kmeans)
-	print(f"clusters_num {clusters_num} silhouette_avg {silhouette_avg}")
+	print(f"for {clusters_num} clusters the silhouette average is {silhouette_avg}")
+	if(best_cluster_num_to_silhouette[1]<silhouette_avg):
+		best_cluster_num_to_silhouette = (clusters_num, silhouette_avg)
+print(f"Highest silhouette score {best_cluster_num_to_silhouette[1]} is achieved in {best_cluster_num_to_silhouette[0]} clusters")
