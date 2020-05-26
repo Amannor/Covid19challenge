196cbd5a0e7518e81c76287eb442f4e9f2a09fdf
Amannor <alon.mannor@gmail.com>
2020-05-26 23:14:11 +0300

    Updated part1 files

--- myCode/submission/part1/find_k_nearest_neighbors.py
+++ myCode/submission/part1/find_k_nearest_neighbors.py
@@ -6,12 +6,29 @@ from sortedcontainers import SortedDict
 from scipy.spatial import distance
 import itertools
 
-text_basis = []
-full_path_for_current_distances_file = r'C:\idc\AdvancedML\ex2_and_relevant_lectures\myCode\take1\articles_ids_to_distance.json'
+def find_k_nearest_neighbors(given_id):
+	given_distance_tuple = articles_ids_to_distance[given_id]
+	nearest_neighbors_distance_to_id = SortedDict()
+	for article_id, distance_tuple in articles_ids_to_distance.items():
+		if(article_id != given_id):
+			dst = distance.euclidean(given_distance_tuple, distance_tuple)
+			nearest_neighbors_distance_to_id.setdefault(dst, []).append(article_id)
+	k_nearset = SortedDict(itertools.islice(nearest_neighbors_distance_to_id.items(), k))
+	neighbors_until_now = 0
+	print(f"nearest {k} neighbors to {given_id}:")
+	for neighbor_distance, article_ids in k_nearset.items():
+		if neighbors_until_now>=k:
+			break
+		neighbors_until_now = neighbors_until_now + len(article_ids)
+		print(f"Distance: {round(neighbor_distance,2)} {'article id' if len(article_ids)==1 else 'article ids'}: {article_ids}")
+
+dir_path = os.path.dirname(os.path.realpath(__file__))
+full_path_for_current_distances_file = os.path.join(dir_path,"articles_ids_to_distance.json")
+
 
 print("Welcome to part 1 - finding the k nearest articles for a given article + their distance")
 print("Please enter full path of article")
-second_file = input() #e.g.: C:\idc\AdvancedML\ex2_and_relevant_lectures\myCode\articles_with_full_text\custom_license\custom_license\pdf_json\ffdd6ba963bcbed1b9e7a293b06529bb4da989b6.json
+second_file_full_path = input() #e.g.: C:\idc\AdvancedML\ex2_and_relevant_lectures\myCode\articles_with_full_text\custom_license\custom_license\pdf_json\ffdd6ba963bcbed1b9e7a293b06529bb4da989b6.json
 print("Please enter a K fo your choice")
 k = -1
 while k<0:
@@ -22,63 +39,11 @@ while k<0:
 	except ValueError:
 	   print("k must be a nonegative  int!")
 
-def extract_text(file_path):
-    with open(file_path, 'r') as f:
-        cur_file_as_dict = json.load(f)
-    list_of_texts = cur_file_as_dict['body_text']
-    full_text = ' '.join([lof['text'] for lof in list_of_texts])
-    return full_text
-
-def init_basis():
-	basis_paths = [r'C:\idc\AdvancedML\ex2_and_relevant_lectures\myCode\articles_with_full_text\comm_use_subset\comm_use_subset\pdf_json\c63c4d58d170136b8d3b5a66424b5ac3f73a92d9.json', r'C:\idc\AdvancedML\ex2_and_relevant_lectures\myCode\articles_with_full_text\custom_license\custom_license\pdf_json\b2897e1277f56641193a6db73825f707eed3e4c9.json']
-	for basis_path in basis_paths:
-		text = extract_text(basis_path)
-		if text:
-			text_basis.append(text)
-	print(f"len(text_basis) {len(text_basis)}")
-
-
-init_basis()
-
-def find_k_nearest_neighbors(given_article_id_to_distance):
-	given_id = list(given_article_id_to_distance.keys())[0]
-	given_distance_tuple = given_article_id_to_distance[given_id]
-	nearest_neighbors_distance_to_id = SortedDict()
-	for article_id, distance_tuple in articles_ids_to_distance.items():
-		if(article_id != given_id):
-			dst = distance.euclidean(given_distance_tuple, distance_tuple)
-			nearest_neighbors_distance_to_id[dst] = article_id
-	print(f"nearest_k_neighbors_distance_to_id {SortedDict(itertools.islice(nearest_neighbors_distance_to_id.items(), k))}")
-
-
-
-
-second_text = extract_text(second_file)
-
-# now doing gzip operation on each option
-zipped_second_text = gzip.compress(bytes(second_text,'utf-8'))
-second_article_id = ntpath.basename(second_file)
-distances_to_basis = []
-cur_article_id_to_distance = dict()
-for first_text in text_basis:
-	zipped_first_text = gzip.compress(bytes(first_text,'utf-8'))
-	texts_concat = first_text + second_text
-
-	zipped_both_texts = gzip.compress(bytes(texts_concat,'utf-8'))
-
-	# What if we want to focus on both documents and not a specific one (Jensen-Shannon alike)
-	both_texts = zipped_first_text + zipped_second_text
-	combined_distance = len(both_texts) - len(zipped_both_texts)
-	distances_to_basis.append(combined_distance)
-cur_distance = tuple(distances_to_basis)
-cur_article_id_to_distance[os.path.splitext(ntpath.basename(second_file))[0]] = cur_distance
-print(f"{cur_article_id_to_distance}")
-
+second_article_id = os.path.splitext(ntpath.basename(second_file_full_path))[0]
 with open(full_path_for_current_distances_file, 'r') as myfile:
     distances_data=myfile.read()
 articles_ids_to_distance = json.loads(distances_data)
 
-find_k_nearest_neighbors(cur_article_id_to_distance)
-
+find_k_nearest_neighbors(second_article_id)
 
 
